5a8198ba6fc4fb22bce9f64fbda923458296cc1a
andrewmalley725 <andrew.alley99@gmail.com>
2023-12-29 23:04:20 -0600

    ability to add expenses or paydays

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from 'axios';
 
 
@@ -6,6 +6,8 @@ export default function Home({ api }){
     const [data, setData] = useState({accounts: []});
     const [show, setShow] = useState(false);
     const [render, setRender] = useState('');
+    const [account, setAcc] = useState(null);
+    const amount = useRef(0);
 
 
     useEffect(() => {
@@ -14,56 +16,124 @@ export default function Home({ api }){
         })
     }, [api]);
 
-    const renderContent = () => {
+    const handleChange = (e) => {
+        setAcc(e.target.value);
+    }
+
+    const handleClick = () => {
         if (render === 'expense'){
-            return(
-                <div>
-                    Amount: <input type="number"></input><br/>
-                    Account:
-                </div>
-            );
-        }
-        else {
-            return(
-                <div>
-                    Payday: <input type="number"></input><br/>
-                </div>
-            )
+            const body = {
+                userid: localStorage.getItem('userId'),
+                accountid: account,
+                amount: parseInt(amount.current)
+            }
+
+            console.log(body);
+
+            axios.post(`${api}/newTransaction`, body).then(res => {
+                window.location.reload();
+            })
+        } else {
+            const body = {
+                userid: localStorage.getItem('userId'),
+                accountid: account !== 'none' ? account : null,
+                amount: parseInt(amount.current)
+            }
+
+            axios.post(`${api}/payday`, body).then(res => {
+                window.location.reload();
+            })
         }
     }
 
-    console.log(data);
+    const renderContent = () => {
+        if (render === 'expense') {
+            return (
+                <div className="mt-4">
+                    <label htmlFor="expenseAmount" className="form-label">Amount:</label>
+                    <input type="number" id="expenseAmount" className="form-control mb-2" onKeyUp={(e) => amount.current = e.target.value}/>
+    
+                    <label htmlFor="accountSelect" className="form-label">Account:</label>
+                    <select id="accountSelect" className="form-select mb-3" onChange={handleChange}>
+                        <option value="" disabled>Select account</option>
+                        {data.accounts.length > 0 ?
+                            data.accounts.map(rec => (
+                                <option key={rec.accountid} value={rec.accountid}>{rec.account_name}</option>
+                            )) : <></>
+                        }
+                    </select>
+    
+                    <button type="button" className="btn btn-primary" onClick={handleClick}>Submit</button>
+                </div>
+            );
+        } else {
+            return (
+                <div className="mt-4">
+                    <label htmlFor="paydayAmount" className="form-label">Payday:</label>
+                    <input type="number" id="paydayAmount" className="form-control mb-2" onKeyUp={(e) => amount.current = e.target.value}/>
+    
+                    <label htmlFor="accountSelectPayday" className="form-label">Account:</label>
+                    <select id="accountSelectPayday" className="form-select mb-3" onChange={handleChange}>
+                        <option value="" disabled>Select account</option>
+                        <option value="none">none</option>
+                        {data.accounts.length > 0 ?
+                            data.accounts.map(rec => (
+                                <option key={rec.accountid} value={rec.accountid}>{rec.account_name}</option>
+                            )) : <></>
+                        }
+                    </select>
+    
+                    <button type="button" className="btn btn-primary" onClick={handleClick}>Submit</button>
+                </div>
+            );
+        }
+    }
+    
     return(
-        <div>
-            <h1>Welcome {data.user}</h1>
+        <div className="container mt-5">
+            <h1 className="mb-4">Welcome {data.user}</h1>
             <h3>Total balance: ${data.total_balance}</h3>
 
-            <h4>Accounts:</h4>
-            { data.accounts.length > 0 ?
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Weight</th>
-                        <th>Balance</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        data.accounts.map((rec) => {
-                            return(
-                                <tr>
-                                    <td>{rec.account_name}</td>
-                                    <td>%{rec.weight * 100}</td>
-                                    <td>{rec.balance}</td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table> : <h4>You don't have any accounts set up</h4>}
-            <button type="button" onClick={() => {setShow(!show); setRender('expense')}}>Add Expense</button>
-            <div style={{display: show ? 'block' : 'none'}}>
+            <h4 className="mt-4">Accounts:</h4>
+            {data.accounts.length > 0 ? (
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Weight</th>
+                            <th>Balance</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.accounts.map((rec) => (
+                            <tr key={rec.accountid}>
+                                <td>{rec.account_name}</td>
+                                <td>%{rec.weight * 100}</td>
+                                <td>{rec.balance}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <h4>You don't have any accounts set up</h4>
+            )}
+
+            <button 
+                type="button" 
+                className="btn btn-primary mt-4" 
+                onClick={() => { setShow(true); setRender('expense'); setAcc(null) }}
+            >
+                Add Expense
+            </button>
+            <button 
+                type="button" 
+                className="btn btn-success mt-4 ms-2" 
+                onClick={() => { setShow(true); setRender('payday'); setAcc(null) }}
+            >
+                Add Funds
+            </button>
+
+            <div className={show ? 'mt-4' : 'd-none'}>
                 {renderContent()}
             </div>
         </div>
